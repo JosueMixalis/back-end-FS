d548c062044ce961271c9d95bf1266f1f83f2d29
Josue Mixalis <cacciator31@gmail.com>
2024-02-16 14:54:38 -0600

    uibuild

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const requestLogger = (request, response, next) => {
 
 
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
 
